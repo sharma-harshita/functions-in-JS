ece0a74921ff44cc9c43237c7789ed6bea35a5d0
harshita.sharma <harshita.sharma@asksid.ai>
2021-12-23 20:45:55 +0530

    es6 function

--- index.js
+++ index.js
@@ -14,16 +14,19 @@ console.log("hello world");
 
 //II : FUNCTION WITH PARAMETERS
 
-function additionTwoNum (a, b) {    // a and b are known as parameters, they gets used inside the definition of the 
-    let c = 10; 
-    console.log(a+b+c);               // function
-}
+// a and b are known as parameters, they gets used inside the definition of the function 
 
-additionTwoNum("Kiran",11);  
+
+// function additionTwoNum (a, b) {   
+//     let c = 10; 
+//     console.log(a+b+c);               
+// }
+
+// additionTwoNum("Kiran",11);  
 
 // additionTwoNum(12,13)           // 12 and 13 are known as arguments, they gets passed inside the function.
 
-additionTwoNum(2,3);
+// additionTwoNum(2,3);
 
 
 
@@ -32,11 +35,33 @@ additionTwoNum(2,3);
 //Whenever you are having a return statement in your function, then at the time of invokation of the function, do not invoke the function directly , instead of that , assign the invokation of the function to a variable and the value which you are returning from the function will be now stored in this new variable which you have created.
 
 
-function doubleTheValue (num){  
-    const value = num * 2;  
-    return value;
-}
-// doubleTheValue(12);
+// function doubleTheValue (num){  
+//     const value = num * 2;  
+//     return value;
+// }
+// // doubleTheValue(12);
 
-const newValue = doubleTheValue(12);
-console.log("Double function call",newValue);
+// const newValue = doubleTheValue(12);
+// console.log("Double function call",newValue);
+
+
+// function handleSubmit (){
+//     const element = document.getElementById("name");
+//     console.log(element.value);
+// }
+
+
+// regular functions
+
+//  function expression : 
+// You can create a function and assign the function's value to a variable. This is known as function expression.
+const a = function (){
+    return 1;
+}
+
+// arrow function
+const displayUserInfo = () => {     // arrow function came in the latest version of JS : ES6
+    console.log("USER INFORMATION");
+}
+
+
